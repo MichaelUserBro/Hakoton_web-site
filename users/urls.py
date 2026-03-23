52e70a674f5fd9e1ef0ab203af08e263b8b67fb3
MichaelUserBro <sneznyemail@gmail.com>
2026-03-23 23:16:14 +0300

    Сделал таблицу лидеров

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
+    path('leaderboard/', views.leaderboard_view, name='leaderboard'), # Добавь эту строку
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from .models import User
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
@@ -6,3 +7,11 @@ def profile_view(request):
     # Мы берем пользователя прямо из запроса (request.user)
     # Благодаря @login_required, мы уверены, что пользователь вошел в систему
     return render(request, 'users/profile.html', {'user': request.user})
+
+
+def leaderboard_view(request):
+    # .order_by('-points') — сортировка от большего к меньшему
+    # [:10] — берем только первых 10 человек
+    users_top = User.objects.all().order_by('-points')[:100]
+    
+    return render(request, 'users/leaderboard.html', {'users_top': users_top})
