08e620756dbd89b18014cc52c55c8e138632a3a7
MichaelUserBro <sneznyemail@gmail.com>
2026-03-23 23:46:27 +0300

    Сделаль все безапасненько

--- events/signals.py
+++ events/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Participation
+from django.db.models import F
 
 @receiver(post_save, sender=Participation)
 def update_user_points(sender, instance, created, **kwargs):
@@ -12,5 +13,5 @@ def update_user_points(sender, instance, created, **kwargs):
     if instance.status:
         user = instance.user
         # Прибавляем баллы из связанного мероприятия
-        user.points += instance.event.points_award
+        user.points = F('points') + instance.event.points_award
         user.save()
